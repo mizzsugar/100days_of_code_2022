abd38ce1c8afe3552c83dfc366cf961d9ba8233b
Mizuki Sato <mizki.sato.0025@gmail.com>
2022-11-27 12:58:58 +0900

    day3 HitPoint::newの返り値が変わったことを踏まえてテストコードを修正

--- hitpoint/src/lib.rs
+++ hitpoint/src/lib.rs
@@ -1,13 +1,11 @@
 use anyhow::{self, Context};
 use thiserror::Error;
 
-
 #[derive(Error, Debug)]
 enum AppError {
     #[error("invalid argument error: {0}")]
     InvalidArgumentError(String),
 }
-
 
 struct HitPoint {
     value: u16,
@@ -17,11 +15,11 @@ impl HitPoint {
     const MAX: u16 = 999;
 
     pub fn new(value: u16) -> Result<Self, AppError> {
-        // TODO: 自作のInvalidArgumentErrorを定義し、それを発生させる
-        // Rust<Self, InvalidArgumentError>をnewの返り値とする
-        // https://github.com/mizzsugar/100days_of_code_2022/issues/1
         if Self::MAX < value {
-            return Err(AppError::InvalidArgumentError(format!("{}以下を指定してください", Self::MAX)))
+            return Err(AppError::InvalidArgumentError(format!(
+                "{}以下を指定してください",
+                Self::MAX
+            )));
         }
         Ok(HitPoint { value: value })
     }
@@ -33,20 +31,24 @@ mod tests {
 
     #[test]
     fn valid_hit_point() {
-        // TODO: テストコード修正
         let _hit_point_1 = HitPoint::new(0);
-        assert_eq!(_hit_point_1.value, 0);
+        assert!(_hit_point_1.is_ok());
+        assert_eq!(_hit_point_1.ok().unwrap().value, 0);
 
-        // let _hit_point_2 = HitPoint::new(1);
-        // assert_eq!(_hit_point_2.value, 1);
+        let _hit_point_2 = HitPoint::new(1);
+        assert!(_hit_point_2.is_ok());
+        assert_eq!(_hit_point_2.ok().unwrap().value, 1);
 
-        // let _hit_point_3 = HitPoint::new(999);
-        // assert_eq!(_hit_point_3.value, 999);
+        let _hit_point_3 = HitPoint::new(999);
+        assert!(_hit_point_3.is_ok());
+        assert_eq!(_hit_point_3.ok().unwrap().value, 999);
     }
 
     #[test]
-    #[should_panic(expected = "999以下を指定してください")]
     fn over_max_hit() {
-        let hit_point_3 = HitPoint::new(1000);
+        let _hit_point_3 = HitPoint::new(1000);
+        // TODO: 自作した例外のテストが発生しているかのテストを書く
+        // https://github.com/mizzsugar/100days_of_code_2022/issues/2
+        assert!(_hit_point_3.is_err());
     }
 }
